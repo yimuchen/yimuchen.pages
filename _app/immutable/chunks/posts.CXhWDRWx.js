e234159e09d519ac63ae2b7192b7a0a39538d3bb
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-04-19 00:58:37 +0200

    Updates

---
+++ _app/immutable/chunks/posts.CXhWDRWx.js
@@ -0,0 +1,1 @@
+const r=t=>{const n=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],e=t.getFullYear(),o=n[t.getMonth()],a=t.getDate();return`${e}.${o}.${a}`};export{r as d};
---
+++ _app/immutable/entry/start.COLTZAcl.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D6qaq_FZ.js";export{t as start};

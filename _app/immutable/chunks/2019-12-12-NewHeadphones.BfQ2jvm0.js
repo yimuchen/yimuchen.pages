c60cfaf0bb7aabb1c434395af1131a4a5d0ae703
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2024-07-04 15:21:22 +0200

    Updates

---
+++ _app/immutable/chunks/2019-12-12-NewHeadphones.BfQ2jvm0.js
@@ -0,0 +1,70 @@
+import{s as $,n as N}from"./scheduler.D0k7T8uo.js";import{S as ee,i as te,e as o,s as l,c as a,g as s,a as h,b as B,d as n,f as i}from"./index.B7LT_aZO.js";function ne(Z){let r,U='<ol class="toc-level toc-level-1"><li class="toc-item toc-item-h2"><a class="toc-link toc-link-h2" href="#choosing-the-headphone">Choosing the headphone</a></li><li class="toc-item toc-item-h2"><a class="toc-link toc-link-h2" href="#my-current-experience">My current experience</a></li><li class="toc-item toc-item-h2"><a class="toc-link toc-link-h2" href="#open-questions-and-closing-words">Open questions and closing words</a></li></ol>',C,m,D=`When travelling on the plane to and from Geneva back in November, I noticed
+that to combat the airplane noise when using my headset, I need to turn the
+volume up to uncomfortable levels, to the point that I was warned by the media
+station on the plane seat. More alarming still, I noticed that I actually got
+used to the volume level after a couple of hours into a movie. As flights would
+likely become more frequent in the coming years of my PhD degree, for the sake
+of my ears, I thought it was time to invest in a set of new noise-cancelling
+headphones.`,I,u,z='<a aria-hidden="true" tabindex="-1" href="#choosing-the-headphone"><span class="icon icon-link"></span></a>Choosing the headphone',_,c,W=`Noise-cancelling headphones have been on my tech-news radar for a while. I know
+that bad implementations of noise-cancelling would lead to a more noisy
+experience and prompt the notorious “cabin-pressure” effect. During the
+thanksgiving season, I had the chance to head over to larger best-buy stores to
+try out a few headsets. I ran into bad implementations (one from skull-candy had
+an annoying hissing noise that is very audible above the leaked in noise), as
+well as being thoroughly impressed with how good noise-cancelling can be in the
+Bose any Sony line up.`,M,g,K=`Noise-cancelling is not the only thing to consider when a headset. How a set of
+headphone sounds is arguably more important for headset. Having tested the
+Sennheiser line-up as having decent/OK noise-cancelling from my trip, decided
+that I liked the Sennheiser’s a bit more than what Bose and Sony had to offer. To
+be entirely honest, during that time I didn’t try out the specific headphone that
+I eventually bought (I only got to try the PXC550 and Momentum 2), but I decided
+to trust the Sennheiser brand, to went for the Sennheiser Momentum 3.`,T,d,F='<a aria-hidden="true" tabindex="-1" href="#my-current-experience"><span class="icon icon-link"></span></a>My current experience',q,f,G=`First and foremost, I don’t have the budget to tryout headphone on a regular
+(my previous headset was an Audio Technica ATH-SJ33 bought over 3 years ago),
+so I probably have no idea what I am talking about when it comes to critiquing
+the audio quality. That being said, I really do like the way they sound.
+Whether it is using the 3.5 mm line to connect to my PC/laptop, or using the
+USB type-C cable to use the headphones in built DAC, they both contain
+significantly more details than my previous headset. The 3.5 mm cable does make
+the output quieter (the impedance quoted from the product page is 450 Ohms, so
+most likely the DAC on my computer is struggling to drive them), but still
+within acceptable ranges. As to how good they are compared to other headsets, I
+have no way of comparing, so no more comments on sound quality.`,H,w,J=`The noise-cancelling capability is as tested: decent/OK. The loudest source of
+consistent noise in my room is the AC unit of our apartment, while the Sennheiser
+blocks most of the rumbling low-frequency noise very well, the higher frequency
+noise still gets in as a slight hiss that slightly audible when there is audio
+playing. This is still very acceptable, as previously when the AC kicks in, I’m
+nearly always tempted to dial up my volume because it in the way of what I was
+listening, whereas now, it is more of a brief pause when the hissing sounds up
+then thinking to myself: “Oh, I guess the AC kicked in”, the moving on. So all in
+all, the noise-cancelling is doing its job.`,S,y,V=`[Update 2019-12-30] On my plane ride back to Taiwan, the noise-cancelling is
+very serviceable, with the low frequency rumbling being clearly blocked out,
+reducing the engine noise to a low volume, higher-pitched faint hissing that is
+unobtrusive to movie viewing experiences and definitely saved ear pains due to
+uncomfortably loud volumes. The office-oriented design of not being able to
+fold flat of the Momentum 3 does make wearing the headphones rather unwieldy
+without putting the headphones away entirely, but it didn’t really matter for
+my using habits.`,A,b,X=`Now for a random story. When operating in wired mode, whether USB or the 3.5 mm
+audio line, there was a strange popping noise in the left ear. It took me a
+week to work out, but it turns out it as a peculiar ground loop issue with my
+computer and the power-line network unit that somehow only the left ear was
+picking up. Moving the power-line unit to another outlet solved the issue, but
+I’m not sure if this is a quirk of higher end audio gear, or just something
+with particular with the Momentum 3. But that was a strange issue that I needed
+solving.`,P,v,E=`[Update 2019-12-30] Another peculiar episode occurred when attempting to
+playback lossless audio files. Weird and irregular popping sounds started
+appearing every 1 minute or so, which indicated to me a buffering problem. It
+has since been fixed using <a href="https://wiki.archlinux.org/index.php/PulseAudio/Troubleshooting#Setting_the_default_fragment_number_and_buffer_size_in_PulseAudio" rel="nofollow">pulseaudio settings</a>, but just note that
+this is yet another issue that one might need to deal with.`,L,p,Q='<a aria-hidden="true" tabindex="-1" href="#open-questions-and-closing-words"><span class="icon icon-link"></span></a>Open questions and closing words',j,x,R=`[Update 2019-12-30] There is a strange bug that after the firmware update of
+in early December, the Momentum 3 just doesn’t work in USB mode with Linux, I
+avoided this thanks to the second unit I got to fix the broken button. I’m not
+sure if this is fixed in future updates, as the Sennheiser mobile app is now
+bugged out with two Momentum devices listed in the app that both cannot be
+connected. I’m happy with the headphone interface as is, so I wouldn’t be
+attempting to solve this anytime soon (I’m more old-fashioned when it comes
+media control, and prefer to control everything manually instead of through
+smart gestures or headphone interfaces).`,O,k,Y=`All in all, I would say I am happy with the overall experience, but at the same
+time, I have a hard time recommending this particular product, with a weird
+episode of the ground loop taking over a week to diagnose, and the first unit
+that I received had a broken volume down button. As a Linux user, I’m no stranger
+to debugging random issues, but that doesn’t make it less frustrating when
+working with the product what I paid a considerable amount to buy.`;return{c(){r=o("nav"),r.innerHTML=U,C=l(),m=o("p"),m.textContent=D,I=l(),u=o("h2"),u.innerHTML=z,_=l(),c=o("p"),c.textContent=W,M=l(),g=o("p"),g.textContent=K,T=l(),d=o("h2"),d.innerHTML=F,q=l(),f=o("p"),f.textContent=G,H=l(),w=o("p"),w.textContent=J,S=l(),y=o("p"),y.textContent=V,A=l(),b=o("p"),b.textContent=X,P=l(),v=o("p"),v.innerHTML=E,L=l(),p=o("h2"),p.innerHTML=Q,j=l(),x=o("p"),x.textContent=R,O=l(),k=o("p"),k.textContent=Y,this.h()},l(e){r=a(e,"NAV",{class:!0,"data-svelte-h":!0}),s(r)!=="svelte-1s3r4zm"&&(r.innerHTML=U),C=h(e),m=a(e,"P",{"data-svelte-h":!0}),s(m)!=="svelte-1c145js"&&(m.textContent=D),I=h(e),u=a(e,"H2",{id:!0,"data-svelte-h":!0}),s(u)!=="svelte-10io6vf"&&(u.innerHTML=z),_=h(e),c=a(e,"P",{"data-svelte-h":!0}),s(c)!=="svelte-cgagij"&&(c.textContent=W),M=h(e),g=a(e,"P",{"data-svelte-h":!0}),s(g)!=="svelte-cxbpdp"&&(g.textContent=K),T=h(e),d=a(e,"H2",{id:!0,"data-svelte-h":!0}),s(d)!=="svelte-7uhobb"&&(d.innerHTML=F),q=h(e),f=a(e,"P",{"data-svelte-h":!0}),s(f)!=="svelte-17yyrox"&&(f.textContent=G),H=h(e),w=a(e,"P",{"data-svelte-h":!0}),s(w)!=="svelte-10ix7s3"&&(w.textContent=J),S=h(e),y=a(e,"P",{"data-svelte-h":!0}),s(y)!=="svelte-1jrzm9p"&&(y.textContent=V),A=h(e),b=a(e,"P",{"data-svelte-h":!0}),s(b)!=="svelte-1a5caqd"&&(b.textContent=X),P=h(e),v=a(e,"P",{"data-svelte-h":!0}),s(v)!=="svelte-18pmkmo"&&(v.innerHTML=E),L=h(e),p=a(e,"H2",{id:!0,"data-svelte-h":!0}),s(p)!=="svelte-jpw9uq"&&(p.innerHTML=Q),j=h(e),x=a(e,"P",{"data-svelte-h":!0}),s(x)!=="svelte-1kz3ty"&&(x.textContent=R),O=h(e),k=a(e,"P",{"data-svelte-h":!0}),s(k)!=="svelte-rw18iv"&&(k.textContent=Y),this.h()},h(){B(r,"class","toc"),B(u,"id","choosing-the-headphone"),B(d,"id","my-current-experience"),B(p,"id","open-questions-and-closing-words")},m(e,t){n(e,r,t),n(e,C,t),n(e,m,t),n(e,I,t),n(e,u,t),n(e,_,t),n(e,c,t),n(e,M,t),n(e,g,t),n(e,T,t),n(e,d,t),n(e,q,t),n(e,f,t),n(e,H,t),n(e,w,t),n(e,S,t),n(e,y,t),n(e,A,t),n(e,b,t),n(e,P,t),n(e,v,t),n(e,L,t),n(e,p,t),n(e,j,t),n(e,x,t),n(e,O,t),n(e,k,t)},p:N,i:N,o:N,d(e){e&&(i(r),i(C),i(m),i(I),i(u),i(_),i(c),i(M),i(g),i(T),i(d),i(q),i(f),i(H),i(w),i(S),i(y),i(A),i(b),i(P),i(v),i(L),i(p),i(j),i(x),i(O),i(k))}}}const ae={title:"Sennheiser Momentum 3 - My thoughts",description:"Initial thoughts on my new headphones after purchase",tags:["hardware","thoughts"]};class se extends ee{constructor(r){super(),te(this,r,null,ne,$,{})}}export{se as default,ae as metadata};
---
+++ _app/immutable/chunks/system.BnwOIUTv.js
@@ -0,0 +1,10 @@
+import{s as x,n as c}from"./scheduler.D0k7T8uo.js";import{S as w,i as b,e as h,s as v,c as m,g as u,a as g,b as _,d as n,f as o}from"./index.B7LT_aZO.js";function L(y){let t,f='<ol class="toc-level toc-level-1"></ol>',l,s,d=`I’ve started using Linux based systems for my daily machine since 2011, though
+most of my understandings for navigating and managing Unix systems is based on
+single-user use-cases. My primary system had been <a href="https://archlinux.org/" rel="nofollow">Arch Linux</a>
+(though I’m slowly transitioning to <a href="https://nixos.org/" rel="nofollow">NixOS</a>), so I have quite a bit of
+experience with fiddling with various system level settings. My choice for
+common tools can be found <a href="https://github.com/yimuchen/dotfiles" rel="nofollow">here</a>.`,r,a,p=`I have worked with quite a bit of command line tools with command line pipings,
+though for complicated tasks, I typically convert to python scripts to have a
+more structured program. A core command line tool that I am horrible at is
+<a href="https://git-scm.com/" rel="nofollow">git</a>: though I have some experience with multi-user collaboration, it
+often defaults to <a href="@shttps://xkcd.com/1597/">this solution</a> when edge cases arise.`;return{c(){t=h("nav"),t.innerHTML=f,l=v(),s=h("p"),s.innerHTML=d,r=v(),a=h("p"),a.innerHTML=p,this.h()},l(e){t=m(e,"NAV",{class:!0,"data-svelte-h":!0}),u(t)!=="svelte-11ukb7v"&&(t.innerHTML=f),l=g(e),s=m(e,"P",{"data-svelte-h":!0}),u(s)!=="svelte-xo2m7h"&&(s.innerHTML=d),r=g(e),a=m(e,"P",{"data-svelte-h":!0}),u(a)!=="svelte-hwf4da"&&(a.innerHTML=p),this.h()},h(){_(t,"class","toc")},m(e,i){n(e,t,i),n(e,l,i),n(e,s,i),n(e,r,i),n(e,a,i)},p:c,i:c,o:c,d(e){e&&(o(t),o(l),o(s),o(r),o(a))}}}const H={name:"unix",title:"Unix systems - maintainance and navigation",proficiency:"Advanced",logo:"terminal.svg"};class T extends w{constructor(t){super(),b(this,t,null,L,x,{})}}export{T as default,H as metadata};

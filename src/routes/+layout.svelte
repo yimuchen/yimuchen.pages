ec04ff5e761cd578f17188c8d00413b8e9af4663
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2024-07-04 15:32:37 +0200

    Fixed favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,13 @@
   // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
   import { config } from "@fortawesome/fontawesome-svg-core";
   import "@fortawesome/fontawesome-svg-core/styles.css";
+import { base } from "$app/paths";
   config.autoAddCss = false;
 </script>
 
+<svelte:head>
+   <link rel="icon" type="image/x-icon" href="{base}/favicon.ico">
+</svelte:head>
 <Header />
 
 <main>
